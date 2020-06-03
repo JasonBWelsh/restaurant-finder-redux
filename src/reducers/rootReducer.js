180c24afd2ed6a9821efc4a59165fe0e76297a87
Jason Welsh <jasonbwelsh@gmail.com>
2020-06-03 10:53:56 -0400

    redux dev tools added

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+  data: [],
+  isLoading: false,
+  isError: false,
+};
+
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_RESTAURANTS_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'RESTAURANTS_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'RESTAURANTS_FETCH_FAILURER':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      return state;
+  }
+};
